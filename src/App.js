5de7087ded8dec8926fdc807257fb1f0fd24154f
ezram <ezramagagane@gmail.com>
2024-09-26 11:35:03 +0200

    admin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
-import GenerateReport from './screens/adminGenerateReport';
-
+//import GenerateReport from './screens/adminGenerateReport';
+import IssueByStatusReport from './screens/issue_by_status_report';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
     <div className="container">
-     <GenerateReport/>
+     <IssueByStatusReport/>
     </div>
  </div>
   );

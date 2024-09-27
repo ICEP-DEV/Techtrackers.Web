4ad75b914c58c395e3aac264a80b69e73a9a774b
ezram <ezramagagane@gmail.com>
2024-09-27 06:53:26 +0200

    ""

--- src/screens/Header.jsx
+++ src/screens/Header.jsx
@@ -1,29 +1,65 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../style/header.css";
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (<div id="header">
-        <nav className="navbar">
-            <div className="navbar-brand">
-                <img src={require('./logo-removebg.jpg')} alt="logo" className="logo" height={100}/>
-            </div>
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-            <div className="nav-links">
-        
-        <Link to='/homePage' className="nav_button">Home</Link>
-        <Link to='/About' className="nav_button">About</Link>
-        <Link to='/service' className="nav_button">Service</Link>
-        <Link to='/'  className="nav_button">Login</Link>
-        <Link to='/contact'  className="nav_button">Contact</Link>
+    window.addEventListener("scroll", onScroll);
 
-       
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div id="header" className={scrolled ? "header-scrolled" : ""}>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <img
+            src={require("./logo-removebg.jpg")}
+            alt="logo"
+            className="logo"
+            height={100}
+          />
         </div>
-        </nav>
-    </div>)
 
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <span className="hamburger-icon">&#9776;</span> {/* Hamburger icon */}
+        </div>
 
+        <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+          <Link to="/homePage" className="nav_button">
+            Home
+          </Link>
+          <Link to="/About" className="nav_button">
+            About
+          </Link>
+          <Link to="/service" className="nav_button">
+            Service
+          </Link>
+          <Link to="/" className="nav_button">
+            Login
+          </Link>
+          <Link to="/contact" className="nav_button">
+            Contact
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;

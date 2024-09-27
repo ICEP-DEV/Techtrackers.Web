b6dbe6b6271d6d5bca65ebf9febebc36306c53a6
ezram <ezramagagane@gmail.com>
2024-09-27 13:44:27 +0200

    ""

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PrinterNotWorking from './pages/PrinterNotWorking';
 import StaffHeader from './components/StaffHeader';
 import './style/style.css';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AppContent = () => {
     const navigate = useNavigate();
@@ -112,3 +113,6 @@ const App = () => (
 );
 
 export default App;
+
+
+

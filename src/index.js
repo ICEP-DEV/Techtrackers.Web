8785f159ada70f474c242746e4d5f7db83aa7422
ezram <ezramagagane@gmail.com>
2024-09-29 16:14:08 +0200

    ""

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import IssueTracker from './components/IssueTracker';
-//import StaffPageDisplay from './screens/StaffPageDisplay';
+import StaffPageDisplay from './screens/StaffPageDisplay';
 import reportWebVitals from './reportWebVitals';
 //import { Sidebar } from 'lucide-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StaffPageDisplay />
   </React.StrictMode>
 );
 

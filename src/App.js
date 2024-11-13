f2a6ba7e62bd587d069bcf0f979ec96f44e62a3f
PhindileT <phndile.simelane250@gmail.com>
2024-11-13 13:42:42 +0200

    manage logs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import HeadDepartment from './components/HeadDepartment';  // Import the routing component
 import HODDashboard from './components/HODDashboard';
 import LogIssueForm from './components/LogIssueFormFolder/LogIssueForm';
-
+import ManageLogs from './components/ManageLogsFolder/ManageLogs';
 function App() {
   return (
       <div className="App">
-        <HeadDepartment />
+        <ManageLogs />
       </div>
    
   );
--- src/components/HeadDepartment.js
+++ src/components/HeadDepartment.js
@@ -1,13 +1,9 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Header';
 import Sidebar from './Sidebar';
 import HODDashboard from './HODDashboard';
-import LogIssueForm from './LogIssueFormFolder/LogIssueForm'
-// import ManageLogs from './components/ManageLogs';
-// import GenerateReport from './components/GenerateReport';
-// import Notifications from './components/Notifications';
+import LogIssueForm from './LogIssueFormFolder/LogIssueForm';
 
 function HeadDepartment() {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
@@ -21,17 +17,11 @@ function HeadDepartment() {
       <div className="app">
         <Header />
         <div className="main-content">
-          <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+          <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
           <div className="content">
             <Routes>
               <Route path="/" element={<HODDashboard isSidebarOpen={isSidebarOpen} />} />
-              {/*<Route path="/log-issue" element={<LogIssueForm />} />
-               <Route path="/manage-logs" element={<ManageLogs />} />
-              <Route path="/generate-report" element={<GenerateReport />} />
-              <Route path="/notifications" element={<Notifications />} /> */}
-
-
-
+              <Route path="/log-issue" element={<LogIssueForm isSidebarOpen={isSidebarOpen} />} />
             </Routes>
           </div>
         </div>

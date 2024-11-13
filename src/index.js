f0bbcd300b9f2bb5c087ecf55efddb989926499a
Sekgotodi <164962370+Sekgotodi@users.noreply.github.com>
2024-11-13 11:46:09 +0200

    add the generate report component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import generateReport from './screens/AdminGenerateReport/generateReport.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

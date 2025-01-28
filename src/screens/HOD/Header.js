79bc9702ce6b5372e9ea37b0e91da1c6520ce41c
PhindileT <phndile.simelane250@gmail.com>
2025-01-28 14:13:48 +0200

    updated css in hod sidebar & dashboard

--- src/screens/HOD/Header.js
+++ src/screens/HOD/Header.js
@@ -1,26 +1,20 @@
 import React from "react";
-import "./Header.css"; // Import your CSS styling for the header
+import styles from "./Header.module.css"; // Import CSS module
 import profileIcon from "./Assets/Profile.png";
 import logo from "./Assets/tut_logo 2.png";
 
-
-
 export default function Header() {
   return (
-    <header className="header">
-      
-      <div className="header-logo">
-        <img src={logo} alt="Logo"  className="TUTlogo"/>
+    <header className={styles.header}>
+      <div className={styles.headerLogo}>
+        <img src={logo} alt="Logo" className={styles.TUTlogo} />
       </div>
 
-      <div className="header-user">
-    
-        {/*<i className="fas fa-user-circle"></i>  User profile icon */}
-        <img src={profileIcon} alt="Profile Icon" className="profile-icon" />
-        <span className="user-name">HOD</span>
+      <div className={styles.headerUser}>
+        <img src={profileIcon} alt="Profile Icon" className={styles.profileIcon} />
+        <span className={styles.userName}>HOD</span>
         <i className="fas fa-chevron-down"></i> {/* Dropdown arrow */}
       </div>
-      
     </header>
   );
 }
